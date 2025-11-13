b4198341d69b2b834ae04e1b356e9b9783c19f9b
hjpark625 <zero950@gmail.com>
2025-11-13 21:38:46 +0900

    refactor: auth.ts에서 불필요한 코드 제거 및 RTK Query를 통한 상태 관리로 전환

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -1,38 +1,7 @@
-import { createAction, createReducer } from '@reduxjs/toolkit'
-// import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import { createReducer } from '@reduxjs/toolkit'
+import { authRequestAPI } from '@/api/authRequest'
 
-// import { getCsrfToken, getQrAccessValidation } from '@/api/authRequest'
-
-const setCsrfToken = createAction<string>('auth/SET_CSRF_TOKEN')
-const setIsValidated = createAction<boolean>('auth/SET_IS_VALIDATED')
-
-const startGetCsrfToken = createAction('auth/START_GET_CSRF_TOKEN')
-const startQrAccessValidation = createAction<{ expires: string; sig: string }>('auth/START_QR_ACCESS_VALIDATION')
-const successGetCsrfToken = createAction('auth/SUCCESS_GET_CSRF_TOKEN')
-const successQrAccessValidation = createAction('auth/SUCCESS_QR_ACCESS_VALIDATION')
-const failGetCsrfToken = createAction<string>('auth/FAIL_GET_CSRF_TOKEN')
-const failQrAccessValidation = createAction<string>('auth/FAIL_QR_ACCESS_VALIDATION')
-
-// export const getCsrfTokenThunk = createAsyncThunk('auth/GET_CSRF_TOKEN', async (_, { dispatch }) => {
-//   const token = await getCsrfToken()
-//   dispatch(setCsrfToken(token))
-//   return token
-// })
-// export const getQrAccessValidationThunk = createAsyncThunk(
-//   'auth/GET_QR_ACCESS_VALIDATION',
-//   async (params: { sig: string; expires: string }, { dispatch }) => {
-//     try {
-//       const { sig, expires } = params
-//       const message = await getQrAccessValidation(expires, sig)
-//       dispatch(setIsValidated(true))
-//       dispatch(getCsrfTokenThunk())
-//       return message
-//     } catch (err) {
-//       dispatch(setIsValidated(false))
-//       throw err
-//     }
-//   }
-// )
+import type { AxiosBaseQueryError } from '@/api/instance'
 
 interface AuthState {
   csrfToken: string
@@ -48,82 +17,28 @@ const initialState: AuthState = {
   message: ''
 }
 
-// const authSlice = createSlice({
-//   name: 'auth',
-//   initialState,
-//   reducers: {
-//     setCsrfToken: (state, action) => {
-//       state.csrfToken = action.payload
-//     },
-//     setIsValidated: (state, action) => {
-//       state.isValidated = action.payload
-//     }
-//   },
-//   extraReducers: (builder) => {
-//     builder
-//       .addCase(getCsrfTokenThunk.pending, (state) => {
-//         state.status = 'loading'
-//       })
-//       .addCase(getCsrfTokenThunk.fulfilled, (state) => {
-//         state.status = 'success'
-//       })
-//       .addCase(getCsrfTokenThunk.rejected, (state, action) => {
-//         state.status = 'error'
-//         state.message = action.error.message || ''
-//       })
-//       .addCase(getQrAccessValidationThunk.pending, (state) => {
-//         state.status = 'loading'
-//       })
-//       .addCase(getQrAccessValidationThunk.fulfilled, (state) => {
-//         state.status = 'success'
-//       })
-//       .addCase(getQrAccessValidationThunk.rejected, (state, action) => {
-//         state.status = 'error'
-//         state.message = action.error.message || ''
-//       })
-//   }
-// })
-
-// export const { setCsrfToken, setIsValidated } = authSlice.actions
-// export const authReducer = authSlice.reducer
 export const authReducer = createReducer(initialState, (builder) =>
   builder
-    .addCase(setCsrfToken, (state, action) => {
+    .addMatcher(authRequestAPI.endpoints.getCsrfToken.matchPending, (state) => {
+      state.status = 'loading'
+    })
+    .addMatcher(authRequestAPI.endpoints.getCsrfToken.matchFulfilled, (state, action) => {
       state.csrfToken = action.payload
     })
-    .addCase(setIsValidated, (state, action) => {
-      state.isValidated = action.payload
+    .addMatcher(authRequestAPI.endpoints.getCsrfToken.matchRejected, (state, action) => {
+      state.status = 'error'
+      state.message = action.error.message ? action.error.message : 'CSRF 토큰 획득 실패'
     })
-    .addCase(startGetCsrfToken, (state) => {
+    .addMatcher(authRequestAPI.endpoints.getQrAccessValidation.matchPending, (state) => {
       state.status = 'loading'
     })
-    .addCase(successGetCsrfToken, (state) => {
-      state.status = 'success'
+    .addMatcher(authRequestAPI.endpoints.getQrAccessValidation.matchFulfilled, (state) => {
+      state.isValidated = true
     })
-    .addCase(failGetCsrfToken, (state, action) => {
+    .addMatcher(authRequestAPI.endpoints.getQrAccessValidation.matchRejected, (state, action) => {
+      const { data } = action.payload as AxiosBaseQueryError
       state.status = 'error'
-      state.message = action.payload
-    })
-    .addCase(startQrAccessValidation, (state) => {
-      state.status = 'loading'
-    })
-    .addCase(successQrAccessValidation, (state) => {
-      state.status = 'success'
-    })
-    .addCase(failQrAccessValidation, (state, action) => {
-      state.status = 'error'
-      state.message = action.payload
+      state.message = data.message ?? 'QR 인증 실패'
     })
     .addDefaultCase((state) => state)
 )
-
-export {
-  setCsrfToken,
-  setIsValidated,
-  startGetCsrfToken,
-  startQrAccessValidation,
-  successGetCsrfToken,
-  successQrAccessValidation,
-  failGetCsrfToken,
-  failQrAccessValidation
-}
